c1cbdd9cdd41dd5031c28d8d9bbe96331498521e
Alvago-maestro <alvago-maestro@yandex.ru>
2024-08-28 22:50:57 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#1st program
+print(9 ** 0.5 * 5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+bez_prioriteta = (2 * 2 + 2)
+print(bez_prioriteta)
+s_prioritetom = (2 * (2 + 2))
+print(s_prioritetom)
+print(bez_prioriteta == s_prioritetom)
+#4th program
+stroka = '123.456'
+stroka = (float(stroka) * 10 // 1)
+print(int(stroka) % 10)
